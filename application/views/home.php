af573fd8ee2cd2ab5c142ee5388b853113e22da9
Pyai Hein <phein@ucdavis.edu>
2017-03-13 08:06:03 -0700

    working on home screen, added header in templates and trying out materialize css

---
+++ application/views/home.php
@@ -0,0 +1,14 @@
+<html>
+	<body style="background-color: #cfd8dc;">
+      <div class="row">
+        <div class="col s12 m12 l12">
+          <div class="card blue-grey darken-1" style="position: absolute; margin: auto; width: 30%;top: 0; left: 0; bottom: 0; right: 0; height: 20%;">
+            <div class="card-content white-text">
+              <span class="card-title">Welcome</span>
+              <p>The website is under construction. Please come back later.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+   </body>
+</html>
