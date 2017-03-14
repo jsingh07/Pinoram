5e34bfcd4200bb9bb40f9ceefa731329b9296c1e
Pyai Hein <phein@ucdavis.edu>
2017-03-14 16:15:06 -0700

    added javascript in the setup database page for dialogue boxes when clicking button

--- application/views/setupdb/setup.php
+++ application/views/setupdb/setup.php
@@ -1,6 +1,8 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
   <html>
     <body>
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script type="text/javascript" src="/assets/js/materialize.min.js"></script>
       <div class="row">
         <div class="col s12 m6">
           <div class="card blue-grey darken-1">
@@ -9,8 +11,10 @@
               <p>Which tables would you like to install?</p>
             </div>
             <div class="card-action">
-              <a href="<?php echo site_url();?>Setupdb/installAll">All</a>
-              <a href="<?php echo base_url();?>Setupdb/installUser">User</a>
+              <a class="btn" onclick="Materialize.toast('Installed All Tables', 4000)"
+                  href="<?php echo base_url();?>Setupdb/installAll">All</a>
+              <a class="btn" onclick="Materialize.toast('Installed User Table', 4000)"
+                  href="<?php echo base_url();?>Setupdb/installUser">User</a>
             </div>
           </div>
         </div>
@@ -21,8 +25,10 @@
               <p>Which tables would you like to drop?</p>
             </div>
             <div class="card-action">
-              <a href="<?php echo site_url();?>Setupdb/dropAll">All</a>
-              <a href="<?php echo base_url();?>Setupdb/dropUser">User</a>
+              <a class="btn" onclick="Materialize.toast('Dropped All Tables', 4000)"
+                  href="<?php echo site_url();?>Setupdb/dropAll">All</a>
+              <a class="btn" onclick="Materialize.toast('Dropped User Table', 4000)"
+                  href="<?php echo base_url();?>Setupdb/dropUser">User</a>
             </div>
           </div>
         </div>
