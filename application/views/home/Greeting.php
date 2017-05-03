17c43bd0af95b15a3cf58576f268006471584122
Pyai Hein <phein@ucdavis.edu>
2017-05-02 20:05:14 -0700

    fixed welcome page css spacing

--- application/views/home/Greeting.php
+++ application/views/home/Greeting.php
@@ -2,7 +2,7 @@
 <html>
 	<body>
 
-		<div class="row white-text" style="height: 730px; padding-top: 200px; max-width: 1000px; position: relative; background: transparent; ">
+		<div class="row white-text" style="height: 100%; padding-top: 20%; max-width: 1000px; position: relative; background: transparent;">
               <h4 class="center z-depth-5" style="font-size: 5em; font-weight: 900">MEMORIES ON A MAP</h4> <br>
               <h5 class="center z-depth-5" style="font-size: 3em; font-weight: 750">Create an album and upload your pictures to get started. Check out the public albums below!</h5>
 		</div>
